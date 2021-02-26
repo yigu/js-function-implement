e1c00676b3b18317f0e35743cd3c1a2389b7904e
yigu <wyqcando@gmail.com>
2021-02-26 18:21:43 +0800

    convert2Tree更新

--- convert2Tree.js
+++ convert2Tree.js
@@ -20,23 +20,68 @@
  * }
  */
 function convert2Tree(arr) {
-  arr.sort((a,b) => {return a.parentId - b.parentId});
   let res = {};
-    for (let i = 0; i < arr.length; i++){
-      if (arr[i].parentId != undefined) {
-        let root = res;
-        while(root.id != arr[i].parentId) {
-          root = root.children;
+  let map = new Map();
+
+  let find = function(id) {
+    for(let i = 0; i < arr.length; i++) {
+      if (arr[i].id == id)
+        return i;
+    }
+    return -1;
+  }
+
+  let build = function(id) {
+    if (!arr.length) {
+      return;
+    }
+    let pos = find(id);
+    let item = arr[pos];
+    if (item.parentId == undefined) {
+      res.id = item.id;
+      res.name = item.name;
+      map.set(item.id, res);
+      arr.splice(pos, 1);
+    } else {
+      //找parent
+      if (map.has(item.parentId)) {
+        //父已经存在，直接放
+        let parent = map.get(item.parentId)
+        if (!parent.children) {
+          parent.children = {};
         }
-        if (!root.children) {
-          root.children = {};
-        }
-        root.children.id = arr[i].id;
-        root.children.name = arr[i].name;
+        parent.children.id = item.id;
+        parent.children.name = item.name;
+        //存一下
+        map.set(item.id, parent.children);
+        arr.splice(pos, 1);
       } else {
-        res.id = arr[i].id;
-        res.name = arr[i].name;
+        //不存在，先去build父
+        build(item.parentId);
+        build(id);
       }
     }
-    return res;
+  }
+
+  while(arr.length) {
+    build(arr[0].id);
+  }
+
+  return res;
 }
+
+convert2Tree([
+    { id: 1, name: 'i1' },
+    { id: 2, name: 'i2', parentId: 1 },
+    { id: 4, name: 'i4', parentId: 3 },
+    { id: 3, name: 'i3', parentId: 2 },
+    { id: 5, name: 'i5', parentId: 4 },
+  ])
+
+convert2Tree([
+    { id: 1, name: 'i1' },
+    { id: 5, name: 'i5', parentId: 1 },
+    { id: 4, name: 'i4', parentId: 5 },
+    { id: 3, name: 'i3', parentId: 4 },
+    { id: 2, name: 'i2', parentId: 3 },
+  ])
